e46a087263e8921b4304bd6157e2a46440121fbe
Juhi <juhi.saxena@rtcamp.com>
2015-02-26 16:58:14 +0530

    Added all tabs to code

--- DataDrivenUsingPageFactory/src/test/java/pageObjects/rtMedia/rtMedia_PG_POF.java
+++ DataDrivenUsingPageFactory/src/test/java/pageObjects/rtMedia/rtMedia_PG_POF.java
@@ -11,16 +11,90 @@ public class rtMedia_PG_POF {
 	public rtMedia_PG_POF(WebDriver driver) {
 		this.driver = driver;
 	}
-	@FindBy(how=How.ID, using="tab-rtmedia-display")
+
+	@FindBy(how = How.ID, using = "tab-rtmedia-display")
 	public WebElement rtMedia_display;
-	
-	@FindBy(how=How.ID, using="tab-rtmedia-wordpress")
+
+	@FindBy(how = How.ID, using = "tab-rtmedia-wordpress")
 	public WebElement rtMedia_wordpress;
+
+	@FindBy(how = How.ID, using = "tab-rtmedia-bbpress")
+	public WebElement rtMedia_bbpress;
+
+	@FindBy(how = How.ID, using = "tab-rtmedia-bp")
+	public WebElement rtMedia_buddypress;
 	
-	public void NavigateToDisplayTab_Action(){
+	@FindBy(how = How.ID, using = "tab-rtmedia-global-albums")
+	public WebElement rtMedia_defaultalabums;
+	
+	@FindBy(how = How.ID, using = "tab-rtmedia-types")
+	public WebElement rtMedia_types;
+	
+	@FindBy(how = How.ID, using = "tab-rtmedia-sizes")
+	public WebElement rtMedia_imagesizes;
+	
+	@FindBy(how = How.ID, using = "tab-rtmedia-privacy")
+	public WebElement rtMedia_privacy;
+	
+	@FindBy(how = How.ID, using = "tab-rtmedia-custom-css-settings")
+	public WebElement rtMedia_customcss;
+	
+	@FindBy(how = How.ID, using = "tab-rtmedia-reward-points")
+	public WebElement rtMedia_rewardpoints;
+	
+	@FindBy(how = How.ID, using = "tab-rtmedia-moderation")
+	public WebElement rtMedia_moderation;
+	
+	@FindBy(how = How.ID, using = "tab-rtmedia-general")
+	public WebElement rtMedia_othersettings;
+	
+
+
+	public void NavigateToDisplayTab_Action() {
 		rtMedia_display.click();
 	}
-	public void NavigateToWordpressTab_Action(){
+
+	public void NavigateToWordpressTab_Action() {
 		rtMedia_wordpress.click();
+	}
+	
+	public void NavigateTobbressTab_Action() {
+		rtMedia_bbpress.click();
+	}
+	
+	public void NavigateTobuddyressTab_Action() {
+		rtMedia_buddypress.click();
+	}
+	
+	public void NavigateToDefaultAlbumsTab_Action() {
+		rtMedia_defaultalabums.click();
+	}
+	
+	public void NavigateToTypesTab_Action() {
+		rtMedia_types.click();
+	}
+	
+	public void NavigateToImageSizesTab_Action() {
+		rtMedia_imagesizes.click();
+	}
+	
+	public void NavigateToPrivacyTab_Action() {
+		rtMedia_privacy.click();
+	}
+	
+	public void NavigateToCustomCSSTab_Action() {
+		rtMedia_customcss.click();
+	}
+	
+	public void NavigateToRewardPointsTab_Action() {
+		rtMedia_rewardpoints.click();
+	}
+	
+	public void NavigateToModerationTab_Action() {
+		rtMedia_moderation.click();
+	}
+	
+	public void NavigateToOtherSettingsTab_Action() {
+		rtMedia_othersettings.click();
 	}
 }
